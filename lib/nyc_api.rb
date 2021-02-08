9ca4492ce4641011e53d012bbcd1a6a77c0cc815
Nate <7nater.nelson@gmail.com>
2021-02-07 21:46:56 -0700

    commit tree ee91f89cbcfb2596b85cd544f1cbc70746050a73

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,18 +1,29 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
+require 'pry'
  
 class GetPrograms
 
-  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+  URL = "https://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
   def get_programs
     uri = URI.parse(URL)
+    
     response = Net::HTTP.get_response(uri)
     response.body
+    # binding.pry 
   end
-
+  def program_school
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      program["agency"]
+    end
+  end
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq
